6bf22de4c95b9feb32bc6851f921722b2e3868f1
DAVID <owolagbadavid@gmail.com>
2023-10-24 19:44:55 +0100

    fix

--- sort.h
+++ sort.h
@@ -31,6 +31,7 @@ void swap_nodes(listint_t **head, listint_t **node);
 size_t list_length(listint_t *head);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
+void merge(int *array, size_t size, int *left, size_t right_size);
 void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
